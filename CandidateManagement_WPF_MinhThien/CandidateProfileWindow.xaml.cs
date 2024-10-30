f904cdaf9967b8db3d6effc75c6da082fd44b2b7
MTh1enn <thientnmse173541@fpt.edu.vn>
2024-10-30 10:04:00 +0700

    Add project files.

---
+++ CandidateManagement_WPF_MinhThien/CandidateProfileWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using Candidate_BusinessObjects;
+using Candidate_Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CandidateManagement_WPF_MinhThien
+{
+    /// <summary>
+    /// Interaction logic for CandidateProfileWindow.xaml
+    /// </summary>
+    public partial class CandidateProfileWindow : Window
+    {
+        private ICandidateProfileService profileService;
+        private IJobPostingService jobPostingService;
+        public CandidateProfileWindow()
+        {
+            InitializeComponent();
+            profileService = new CandidateProfileService();
+            jobPostingService = new JobPostingService();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            loadDataInit();
+        }
+
+        private void loadDataInit()
+        {
+            this.dtgCandidateProfile.ItemsSource = profileService.GetCandidateProfiles();
+            this.cmbPostID.ItemsSource = jobPostingService.GetJobPostings();
+            this.cmbPostID.DisplayMemberPath = "JobPostingTitle";
+            this.cmbPostID.SelectedValuePath = "PostingId";
+
+            txtCandidateID.Text = "";
+            txtBirthDay.Text = "";
+            txtDescription.Text = "";
+            txtImageURL.Text = "";
+            txtFullName.Text = "";
+            cmbPostID.SelectedValue = "";
+        }
+
+        private void dtgCandidateProfile_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid dataGrid = sender as DataGrid;
+            DataGridRow row = dataGrid.ItemContainerGenerator
+                .ContainerFromIndex(dataGrid.SelectedIndex) as DataGridRow;
+            if (row != null)
+            {
+                DataGridCell RowColumn = dataGrid.Columns[0].GetCellContent(row).Parent
+                    as DataGridCell;
+                string id = ((TextBlock)RowColumn.Content).Text;
+                CandidateProfile profile = profileService.GetCandidateProfileById(id);
+                if (profile != null)    
+                {
+                    txtCandidateID.Text = profile.CandidateId;
+                    txtBirthDay.Text = profile.Birthday.ToString();
+                    txtDescription.Text = profile.ProfileShortDescription;
+                    txtImageURL.Text = profile.ProfileUrl;
+                    txtFullName.Text = profile.Fullname;
+                    cmbPostID.ItemsSource = jobPostingService.GetJobPostings();
+                    cmbPostID.SelectedValue = profile.PostingId;
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            CandidateProfile candidate = new CandidateProfile();
+            candidate.CandidateId = txtCandidateID.Text;
+            candidate.Fullname = txtFullName.Text;
+            candidate.Birthday = DateTime.Parse(txtBirthDay.Text);
+            candidate.ProfileUrl = txtImageURL.Text;
+            candidate.PostingId = cmbPostID.SelectedValue.ToString();
+            candidate.ProfileShortDescription = txtDescription.Text;
+            if (profileService.AddCandidateProfile(candidate))
+            {
+                MessageBox.Show("Add Successfully");
+                loadDataInit();
+            }
+            else
+            {
+                MessageBox.Show("Create Fail!!!");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            CandidateProfile candidate = new CandidateProfile();
+            candidate.CandidateId = txtCandidateID.Text;
+            candidate.Fullname = txtFullName.Text;
+            candidate.Birthday = DateTime.Parse(txtBirthDay.Text);
+            candidate.ProfileUrl = txtImageURL.Text;
+            candidate.PostingId = cmbPostID.SelectedValue.ToString();
+            candidate.ProfileShortDescription = txtDescription.Text;
+            if (profileService.UpdateCandidateProfile(candidate))
+            {
+                MessageBox.Show("Update Successfully");
+                loadDataInit();
+            }
+            else
+            {
+                MessageBox.Show("Update Fail!!!");
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (dtgCandidateProfile.SelectedItem is CandidateProfile selectedProfile)
+            {
+                var result = MessageBox.Show("Are you sure you want to delete this profile?", "Confirm Delete", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result == MessageBoxResult.Yes)
+                {
+                    bool isDeleted = profileService.DeleteCandidateProfile(selectedProfile.CandidateId);
+                    if (isDeleted)
+                    {
+                        MessageBox.Show("Profile deleted successfully!");
+                        loadDataInit();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to delete the profile.");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a profile to delete.");
+            }
+        }
+    }
+}
---
+++ Candidate_DAO/CandidateProfileDAO.cs
@@ -0,0 +1,105 @@
+﻿using Candidate_BusinessObjects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candidate_DAO
+{
+    public class CandidateProfileDAO
+    {
+        private CandidateManagementContext context;
+        private static CandidateProfileDAO instance;
+        public CandidateProfileDAO()
+        {
+            context = new CandidateManagementContext();
+        }
+        public static CandidateProfileDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CandidateProfileDAO();
+                }
+                return instance;
+            }
+        }
+        public CandidateProfile GetCandidateProfileById(string id)
+        {
+            return context.CandidateProfiles.SingleOrDefault(m => m.CandidateId.Equals(id));
+        }
+        public List<CandidateProfile> GetCandidateProfiles()
+        {
+            return context.CandidateProfiles.ToList();
+        }
+        public bool AddCandidateProfile(CandidateProfile candidateProfile)
+        {
+            bool isSuccess = false;
+            CandidateProfile candidate = this.GetCandidateProfileById(candidateProfile.CandidateId);
+            try
+            {
+                if (candidate == null)
+                {
+                    context.CandidateProfiles.Add(candidateProfile);
+                    context.SaveChanges();
+                    isSuccess = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                var errorMessage = ex.Message;
+                if (ex.InnerException != null)
+                {
+                    errorMessage += " Inner Exception: " + ex.InnerException.Message;
+                }
+                throw new Exception(errorMessage);
+            }
+            return isSuccess;
+        }
+
+        public bool DeleteCandidateProfile(string profileID)
+        {
+            bool isSuccess = false;
+            CandidateProfile candidateProfile = this.GetCandidateProfileById(profileID);
+            try
+            {
+                if (profileID != null)
+                {
+                    context.CandidateProfiles.Remove(candidateProfile);
+                    context.SaveChanges();
+                    isSuccess = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return isSuccess;
+        }
+        public bool UpdateCandidateProfile(CandidateProfile candidate)
+        {
+            bool isSuccess = false;
+            try
+            {
+                var existingCandidate = GetCandidateProfileById(candidate.CandidateId);
+
+                if (existingCandidate != null)
+                {
+                    context.Entry(existingCandidate).State = EntityState.Detached;
+                    context.CandidateProfiles.Attach(candidate);
+                    context.Entry(candidate).State = EntityState.Modified;
+                    context.SaveChanges();
+                    isSuccess = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return isSuccess;
+        }
+    }
+}
