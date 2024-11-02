d48028c0acdac680299a6f2c86675c866ffdd316
MTh1enn <thientnmse173541@fpt.edu.vn>
2024-11-02 15:10:33 +0700

    edit UI/UX

--- CandidateManagement_RazorPage_MinhThien/Program.cs
+++ CandidateManagement_RazorPage_MinhThien/Program.cs
@@ -1,25 +1,39 @@
+using Candidate_Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddRazorPages();
+builder.Services.AddSession(); 
+
+
+builder.Services.AddScoped<IHRAccountService, HRAccountService>();
+builder.Services.AddScoped<ICandidateProfileService, CandidateProfileService>();
+builder.Services.AddScoped<IJobPostingService, JobPostingService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSession(); 
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/LoginPages/Login");
+    return Task.CompletedTask;
+});
 
 app.Run();
